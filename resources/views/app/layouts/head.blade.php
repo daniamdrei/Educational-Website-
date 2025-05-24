f3824ff25fe724bc6f6806eefb92a5a5944a5605
[daniamdrei] <[daniamdrei@gmail.com]>
2025-05-24 09:53:08 +0300

    start with user panel

---
+++ resources/views/app/layouts/head.blade.php
@@ -0,0 +1,24 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+
+<head>
+    <meta charset="utf-8">
+    <title>Homelengo - Real Estate HTML Template</title>
+    <meta name="keywords" content="HTML, CSS, JavaScript, Bootstrap">
+    <meta name="description" content="Real Estate HTML Template">
+    <meta name="author" content="themesflat.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+   <!-- font -->
+   <link rel="stylesheet" href="fonts/fonts.css">
+   <!-- Icons -->
+   <link rel="stylesheet" href="appAssets/fonts/font-icons.css">
+   <link rel="stylesheet" href="appAssets/css/bootstrap.min.css">
+   <link rel="stylesheet" href="appAssets/css/swiper-bundle.min.css">
+   <link rel="stylesheet" href="appAssets/css/animate.css">
+   <link rel="stylesheet" type="text/css" href="appAssets/css/styles.css"/>
+
+    <!-- Favicon and Touch Icons  -->
+    <link rel="shortcut icon" href="appAssets/images/logo/favicon.png">
+    <link rel="apple-touch-icon-precomposed" href="appAssets/images/logo/favicon.png">
+
+</head>
